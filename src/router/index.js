a1e6b286a3f073dbf013fd19a40b29f412d5a83b
Smith <122034329+smith-maina7@users.noreply.github.com>
2024-10-09 02:03:12 +0300

    Added the not found page and its functionality of keeping current path on URL

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import data from '@/data.json'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
@@ -9,6 +10,19 @@ const routes = [
     name: 'destination',
     component: () => /* webpackChunkName: "destination" */ import('@/views/DestinationShow.vue'),
     props: ({ params }) => ({ ...router.params, id: parseInt(params.id), slug: params.slug }),
+    beforeEnter: (to, from, next) => {
+      const destination = data.destinations.find((d) => d.id === parseInt(to.params.id))
+      if (!destination) {
+        next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.split('/').slice(1) },
+          query: to.query,
+          hash: to.hash
+        })
+      } else {
+        next()
+      }
+    },
     children: [
       {
         path: ':experienceSlug',
@@ -21,12 +35,20 @@ const routes = [
         })
       }
     ]
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => /* webpackChunkName: "not-found" */ import('@/views/NotFound.vue')
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior: (to, from, savedPosition) => {
+    return savedPosition || { top: 0 }
+  }
 })
 
 export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Not Found</h1>
+
+  <p>
+    Oops were not able to find you page <router-link to="/">home</router-link>. Please try again.
+  </p>
+</template>
